fbb117033f75bb498906d39cde33346542b1543e
Sabana-Yasmine <sabana.platosys@gmail.com>
2022-09-14 15:59:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import FacebookLogin from "react-facebook-login";
 import './App.css';
 
 function App() {
+
+  const [login, setlogin] = useState(false);
+  const[data, setdata] = useState({});
+  const [picture, setpicture] = useState("")
+
+  const responseFacebook = (Response) => {
+    if(Response.status == "unkonwn"){
+      console.log(Response)
+      setlogin(false)
+      return false
+    }
+    setdata(Response)
+    setpicture(Response.picture.data.url)
+    if(Response.accessToken){
+      setlogin(true)
+    }else{
+      setlogin(false)
+    }
+  }
+
+  const logout = () => {
+    setlogin(false)
+    setdata({})
+    setpicture("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {!login && (
+        <FacebookLogin
+          appId="569720507786195"
+          autoLoad={false}
+          fields="name,email,picture"
+          scope="public_profile,email,user_friends"
+          callback={responseFacebook}
+          icon="fa-facebook"
+        />
+      )}
+
+      {login && (
+        <div className="card">
+          <div className="card-body">
+            <img className="rounded" src={picture} alt="Profile" />
+            <h5 className="card-title">{data.name}</h5>
+            <p className="card-text">Email ID: {data.email}</p>
+            <a href="#" className="btn btn-danger btn-sm" onClick={logout}>
+              Logout
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
